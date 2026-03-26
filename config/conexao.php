4051bba44b144ff682f5b933cf4889427bb60420
Guilherme Ferreira <gui.ferreira365@gmail.com>
2026-03-26 17:52:23 -0300

    Corrige erro de variável na conexão
    Altera nome da variável pdo.

--- config/conexao.php
+++ config/conexao.php
@@ -5,8 +5,8 @@ $usuario = "root";
 $senha = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
